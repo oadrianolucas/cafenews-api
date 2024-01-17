43ba1f0ad534b13057dc30867b66a18ff609242d
Adriano Lucas <adrianolucas@MacBook-Pro-de-Adriano.local>
2024-01-17 11:13:58 -0300

    edit post email

--- src/api/beehiiv.js
+++ src/api/beehiiv.js
@@ -1,33 +1,39 @@
 const axios = require('axios');
 const key_beehiiv = ''
 const pub_beehiv = ''
+
 const Beehiiv = {
-    subscribe: async(req, res) =>{
+    subscribe: async (req, res) => {
         const { email } = req.body;
         const index = {
             method: 'GET',
-            url: 'https://api.beehiiv.com/v2/publications/' + pub_beehiv + '/subscriptions',
-            params: { email: email, direction: 'asc' },
+            url: `https://api.beehiiv.com/v2/publications/${pub_beehiv}/subscriptions`,
+            params: { email, direction: 'asc' },
             headers: {
-              Accept: 'application/json',
-              Authorization: 'Bearer ' + key_beehiiv
+                Accept: 'application/json',
+                Authorization: 'Bearer ' + key_beehiiv
             }
-          };
-          try {
+        };
+
+        try {
             const { data } = await axios.request(index);
-            if (data.data[0].status === 'active') {
-                res.status(400).json({'message': 'E-mail já cadastrado.'})
+            if (data.data && data.data.length > 0) {
+                if (data.data[0].status === 'active') {
+                    res.status(400).json({ 'message': 'E-mail já cadastrado.' });
+                } else {
+                    res.status(400).json({ 'message': 'E-mail já cadastrado, mas não ativo.' });
+                }
             } else {
                 const options = {
                     method: 'POST',
-                    url: 'https://api.beehiiv.com/v2/publications/'+ pub_beehiv +'/subscriptions',
+                    url: `https://api.beehiiv.com/v2/publications/${pub_beehiv}/subscriptions`,
                     headers: {
                         'Content-Type': 'application/json',
                         Accept: 'application/json',
                         Authorization: 'Bearer ' + key_beehiiv
                     },
                     data: {
-                        email: email,
+                        email,
                         reactivate_existing: true,
                         send_welcome_email: false,
                         utm_source: 'cafe-newsletter',
@@ -36,17 +42,19 @@ const Beehiiv = {
                         referring_site: 'api.cafenews.com.vr',
                     }
                 };
+
                 try {
                     const response = await axios.request(options);
                     res.status(200).send(response.data);
                 } catch (error) {
-                    res.status(500).send(error);
+                    console.error('Subscription error:', error);
+                    res.status(500).send({ message: 'Error subscribing the user', error: error.message });
                 }
             }
-          } catch (error) {
+        } catch (error) {
             res.status(500).send(error);
-          }
-        
+        }
     }
 }
+
 module.exports = Beehiiv;
