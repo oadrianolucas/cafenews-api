bb74ea30684975044465ddcb43530682e0da619d
Adriano Lucas <adrianolucas@MacBook-Pro-de-Adriano.local>
2024-01-25 20:19:24 -0300

    create unsubscribe

--- src/api/beehiiv.js
+++ src/api/beehiiv.js
@@ -21,7 +21,7 @@ const Beehiiv = {
                 if (data.data[0].status === 'active') {
                     res.status(400).json({ 'message': 'E-mail já cadastrado.' });
                 } else {
-                    res.status(400).json({ 'message': 'E-mail já cadastrado, mas não ativo.' });
+                    res.status(402).json({ 'message': 'E-mail já cadastrado, mas não ativo.' });
                 }
             } else {
                 const options = {
@@ -42,7 +42,6 @@ const Beehiiv = {
                         referring_site: 'api.cafenews.com.vr',
                     }
                 };
-
                 try {
                     const response = await axios.request(options);
                     res.status(200).send(response.data);
@@ -51,6 +50,48 @@ const Beehiiv = {
                     res.status(500).send({ message: 'Error subscribing the user', error: error.message });
                 }
             }
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    },
+    unsubscribe: async (req, res) => {
+        const { email } = req.body;
+        const index = {
+            method: 'GET',
+            url: `https://api.beehiiv.com/v2/publications/${pub_beehiv}/subscriptions`,
+            params: { email, direction: 'asc' },
+            headers: {
+                Accept: 'application/json',
+                Authorization: 'Bearer ' + key_beehiiv
+            }
+        };
+        try {
+            const { data } = await axios.request(index);
+            if (data.data && data.data.length > 0) {
+                if (!data.data[0].email) {
+                    res.status(400).json({ 'message': 'E-mail já cadastrado.' });
+                } else {
+                    const id_sub = data.data[0].id 
+                    const options = {
+                        method: 'DELETE',
+                        url: `https://api.beehiiv.com/v2/publications/${pub_beehiv}/subscriptions/${id_sub}`,
+                        headers: {
+                            'Content-Type': 'application/json',
+                            Accept: 'application/json',
+                            Authorization: 'Bearer ' + key_beehiiv
+                        },
+                    };
+                    try {
+                        const response = await axios.request(options);
+                        res.status(200).send(response.data);
+                    } catch (error) {
+                        console.error('Subscription error:', error);
+                        res.status(500).send({ message: 'Error unsubscribing the user', error: error.message });
+                    }
+                }  
+                } else {
+                    res.status(404).json({ 'message': 'E-mail não localizado em nossa newsletter.' });
+                }
         } catch (error) {
             res.status(500).send(error);
         }
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router()
 
-const { subscribe } = require("./api/beehiiv")
+const { subscribe, unsubscribe } = require("./api/beehiiv")
 
 router.post('/subscribe', subscribe)
+router.delete('/unsubscribe', unsubscribe)
 
 router.get("/", (req, res) => {
     res.status(200).json({ success: "Api Café Newslatter" })
